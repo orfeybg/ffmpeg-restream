88939f0dafe30c71d7ff9373c374558fd0982d59
orfeybg <33177252+orfeybg@users.noreply.github.com>
2019-02-15 18:38:42 +0200

    changes and optimization

--- timedif.sh
+++ timedif.sh
@@ -27,7 +27,7 @@ if (((newseconds - camer1) / 60)) ; then
     sleep 2
     find /var/www/html/livecam/media/camera1/ -type f -exec rm {} \;
     echo "Start camera camera1"
-    screen -Sdm camera1  /home/cameri/livecam/camera1.sh &
-    screen -list | grep camera1 | cut -f1 -d'.' | head -n1 | sed 's/\W//g'  >  /home/cameri/livecam/pids/camera1
+    /home/cameri/livecam/camera1.sh &
+    ps -ef | grep camera1 | grep -v grep | awk '{print $2}'  >  /home/cameri/livecam/pids/camera1
     echo "Camera camera1 e startirana"
 fi
